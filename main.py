eb6db5ee5802fe7e6d82e170fcc39d0caf5d28df
Terry5066 <terencempofu701@gmail.com>
2021-09-26 17:37:37 +0200

    add comment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #converting user input
-inp=input('Europe floor')
-usf=int(inp)+1
-print ('US floor',usf)
+inp = input('Europe floor')
+usf = int(inp) + 1
+print ('US floor', usf)
+
+##ground floor is floor zero in europe
