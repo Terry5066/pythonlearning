6309774d72410d0451778a2c9b91361750fcb6d4
Terry5066 <terencempofu701@gmail.com>
2021-09-28 23:42:49 +0200

    intial commit

---
+++ string methods.py
@@ -0,0 +1,33 @@
+x='hello terence'
+print (x.capitalize())
+
+txt = "36 is my age."
+x = txt.capitalize()
+print (x)
+
+x="my name is terence terence"
+print(x.count("terence"))
+
+x="my name is terence terence"
+print(x.find("name"))
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+print (bool("hello"))
+print (bool(12))
+
+def myfunction():
+    return True
+
+print (myfunction())
+
+if myfunction():
+    print ("yes")
+else:
+    print ("no")
